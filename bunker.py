8bb3294e3a12dd29c2b2477269cc7cec9eae25c7
omarnabulsi16 <31221375+omarnabulsi16@users.noreply.github.com>
2019-10-02 10:27:27 -0700

    another directory fix

--- bunker.py
+++ bunker.py
@@ -7,7 +7,7 @@ class Bunker(Sprite):
         self.screen = screen
         self.screen_rect = screen.get_rect()
         self.ai_settings = ai_settings
-        self.image = pygame.image.load(r'C:\Users\omarn\Desktop\bunker.png')
+        self.image = pygame.image.load('images/bunker.png')
         self.rect = self.image.get_rect()
         self.rect.y = self.screen_rect.bottom * 8/10
         self.rect.x = self.screen_rect.right * x / 6 - self.rect.width / 2
