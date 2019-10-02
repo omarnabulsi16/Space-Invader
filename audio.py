7280995833c534c172bea384cbe38174e2522bd3
omarnabulsi16 <31221375+omarnabulsi16@users.noreply.github.com>
2019-10-02 10:25:46 -0700

    audio directory fix

--- audio.py
+++ audio.py
@@ -5,12 +5,12 @@ class Audio:
         pygame.mixer.init()
         pygame.mixer.set_num_channels(5)
         self.exploa = pygame.mixer.Channel(2)
-        self.explob = pygame.mixer.Sound(r"C:\Users\omarn\Desktop\explosion.wav")
+        self.explob = pygame.mixer.Sound('sounds/explosion.wav')
         self.omin = pygame.mixer.Channel(0)
-        self.omina = pygame.mixer.Sound(r"C:\Users\omarn\Desktop\bk1.wav")
-        self.ominb = pygame.mixer.Sound(r"C:\Users\omarn\Desktop\bk2.wav")
-        self.ominc = pygame.mixer.Sound(r"C:\Users\omarn\Desktop\bk3.wav")
-        self.omind = pygame.mixer.Sound(r"C:\Users\omarn\Desktop\bk4.wav")
+        self.omina = pygame.mixer.Sound('sounds/bk1.wav')
+        self.ominb = pygame.mixer.Sound('sounds/bk2.wav')
+        self.ominc = pygame.mixer.Sound('sounds/bk3.wav')
+        self.omind = pygame.mixer.Sound('sounds/bk4.wav')
     @staticmethod
     def play():
         # background music
